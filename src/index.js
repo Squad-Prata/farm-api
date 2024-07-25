b947de6e9a29da454f5afa680a3ce4fcf8312ec2
Douglas Trindade <douglastrindade22@hotmail.com>
2024-07-25 02:14:30 -0300

    chore: added enviroment variables and added /api userouter on index

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const userRoutes = require('./routes/userRoutes');
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const userRoutes = require("./routes/userRoutes");
 
-const prisma = require('./config/database')
+const prisma = require("./config/database");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.json());
-app.use('/', userRoutes);
+app.use("/api", userRoutes);
 
 const PORT = 3001;
 
