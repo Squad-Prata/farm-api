d1baa77133eac7921b403e821dcd9000ddd5ec1f
Douglas Trindade <douglastrindade22@hotmail.com>
2024-08-05 02:07:59 -0300

    feat: change reset password router name

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,32 +1,44 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
-const sendPasswordController = require('../controllers/sendPasswordController');
-const autheConfig = require('../middlewares/auth');
+const userController = require("../controllers/userController");
+const sendPasswordController = require("../controllers/sendPasswordController");
+const autheConfig = require("../middlewares/auth");
 
 // Cadastros de usuarios
-router.post('/cadastro', userController.registrarUsuario);
+router.post("/cadastro", userController.registrarUsuario);
 
 // Login do usuarios
-router.post('/login', userController.loginUsuario);
+router.post("/login", userController.loginUsuario);
 
 // Buscar de usuarios
-router.get('/usuarios/:id', autheConfig.authenticateToken, userController.buscarUsuarioPorId);
-router.get('/buscartodosusuarios', userController.buscarTodosUsuarios);
+router.get(
+  "/usuarios/:id",
+  autheConfig.authenticateToken,
+  userController.buscarUsuarioPorId
+);
+router.get("/buscartodosusuarios", userController.buscarTodosUsuarios);
 
 // Atualização de cadastro
-router.put('/usuarios/:id', autheConfig.authenticateToken, userController.atualizarUsuario);
+router.put(
+  "/usuarios/:id",
+  autheConfig.authenticateToken,
+  userController.atualizarUsuario
+);
 
 // Deletar usuarios
-router.delete('/usuarios/:id', autheConfig.authenticateToken, userController.deletarUsuario);
+router.delete(
+  "/usuarios/:id",
+  autheConfig.authenticateToken,
+  userController.deletarUsuario
+);
 
 // Redefinição de senha E-mail
-router.post('/enviosenha', sendPasswordController.sendPassword);
+router.post("/recuperar-senha", sendPasswordController.sendPassword);
 
 // Atualização de Senha
-router.post('/atualizarsenha', sendPasswordController.updatePassword);
+router.post("/atualizarsenha", sendPasswordController.updatePassword);
 
 // Inativar usuarios
-router.patch('/usuarios/inativar/:id', userController.inativarUsuario);
+router.patch("/usuarios/inativar/:id", userController.inativarUsuario);
 
 module.exports = router;
