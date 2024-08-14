78dfb470246c36e2012890eb75fae711b073b175
ivanizidoro <ivanizidoro@hotmail.com>
2024-08-13 21:57:51 -0300

    feito alteração cors

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const userRoutes = require("./routes/userRoutes");
 const prisma = require("./config/database");
 const app = express();
 
-app.use(cors(
-  {
-    origin: process.env.FRONTEND_URL,
-    credentials: true
-  }
-));
+const corsOptions = {
+  origin: "https://staging-farmapp.vercel.app/", // substitua pelo domínio do seu frontend na Vercel
+  optionsSuccessStatus: 200
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.json());
 app.use("/", userRoutes);
