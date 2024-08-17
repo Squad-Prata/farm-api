a81b1ca507ad6527f38c18927d0a071613b20526
Douglas Trindade <douglastrindade22@hotmail.com>
2024-08-16 21:39:25 -0300

    feat: change language andpoints to english

--- src/controllers/sendPasswordController.js
+++ src/controllers/sendPasswordController.js
@@ -1,26 +1,25 @@
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require('bcrypt');
-require('dotenv').config();
-const emailsend = require('../services/emailServices');
+const { PrismaClient } = require("@prisma/client");
+const bcrypt = require("bcrypt");
+require("dotenv").config();
+const emailsend = require("../services/emailServices");
 
 const prisma = new PrismaClient();
 
+exports.passwordReset = async (req, res) => {
+  const { email } = req.body;
 
-exports.sendPassword = async (req, res) => {
-    const { email } = req.body;
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(400).send("Usuário não encontrado");
+  }
 
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) {
-        return res.status(400).send('Usuário não encontrado');
-    }
+  const resetLink = `http://localhost:3000/reset-password/${email}`;
 
-    const resetLink = `http://localhost:3000/reset-password/${email}`;
-
-    const mailOptions = {
-        from: 'farmapi119@gmail.com',
-        to: user.email,
-        subject: 'Redefinição de senha',
-        html: `Você está recebendo este e-mail porque você (ou alguém) solicitou a redefinição da senha da sua conta.
+  const mailOptions = {
+    from: "farmapi119@gmail.com",
+    to: user.email,
+    subject: "Redefinição de senha",
+    html: `Você está recebendo este e-mail porque você (ou alguém) solicitou a redefinição da senha da sua conta.
         <br>
         <br>
            <a href="${resetLink}" style="text-decoration:none;">
@@ -31,33 +30,33 @@ exports.sendPassword = async (req, res) => {
           <br>
           </a>
            Se você não solicitou isso, por favor, ignore este e-mail e sua senha permanecerá a mesma.`,
-    };
+  };
 
-    res.status(200).send('Email de redefinição de senha enviado com sucesso');
+  res.status(200).send("Email de redefinição de senha enviado com sucesso");
 
-    emailsend.sendMail(mailOptions, (error, res) => {
-        if (error) {
-            console.error('Erro ao enviar email', error);
-            return res.status(500).send('Erro ao enviar email');
-        }
-    });
+  emailsend.sendMail(mailOptions, (error, res) => {
+    if (error) {
+      console.error("Erro ao enviar email", error);
+      return res.status(500).send("Erro ao enviar email");
+    }
+  });
 };
 
-exports.updatePassword = async (req, res) => {
-    const { password } = req.body;
-    const { email } = req.params;
+exports.passwordUpdate = async (req, res) => {
+  const { password } = req.body;
+  const { email } = req.params;
 
-    const user = await prisma.user.findUnique({ where: { email } });
-    if (!user) {
-        return res.status(400).send('Usuário não encontrado');
-    }
+  const user = await prisma.user.findUnique({ where: { email } });
+  if (!user) {
+    return res.status(400).send("Usuário não encontrado");
+  }
 
-    const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, 10);
 
-    await prisma.user.update({
-        where: { email },
-        data: { password: hashedPassword },
-    });
+  await prisma.user.update({
+    where: { email },
+    data: { password: hashedPassword },
+  });
 
-    res.status(200).send('Senha atualizada com sucesso');
+  res.status(200).send("Senha atualizada com sucesso");
 };
