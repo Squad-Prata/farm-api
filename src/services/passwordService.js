680e86dd11009d1b785d2d17724b796cd6275a9e
Gguife <gguife747@gmail.com>
2024-09-19 23:11:28 -0300

    refactor: Movie file to new directory

---
+++ src/services/passwordService.js
@@ -0,0 +1,16 @@
+const bcrypt = require('bcryptjs');
+
+const generatePassword = () => {
+  const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  let password = '';
+  for (let i = 0; i < 10; i++) {
+    password += chars.charAt(Math.floor(Math.random() * chars.length));
+  }
+  return password;
+};
+
+const hashPassword = async (password) => {
+  return await bcrypt.hash(password, 10);
+};
+
+module.exports = { generatePassword, hashPassword };
